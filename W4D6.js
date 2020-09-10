a0bd38af754bad94e8da82a20b64bf80ada91fb1
aya abuyousef <aya.abuyousef@gmail.com>
2020-09-10 09:59:56 +0300

    W4D6

--- W4D6.js
+++ W4D6.js
@@ -4,3 +4,23 @@
 //Create a function that takes a student as parameter and print out the details of the student
 //Create a function that takes the array of students and sort them out based on age (younger-to-older)
 //Create a function that takes the array and a string either “M” or “F” and returns the male students if “M” was passed and female students if “F” was passed
+function Student(Name,Age,DateOfBirth,Gender){
+    return {
+        name:Name,
+        age:Age,
+        DateOfBirth:DateOfBirth,
+        gender:Gender,
+    }
+   }
+
+   ////Create an array and fill it with 5 students
+
+   var array=[]
+   array.push(student('aya',1,'1/1/2020','female'))
+   array.push(student('ala',2,'1/2/2020','female'))
+   array.push(student('alma',3,'1/3/2020','female'))
+   array.push(student('naya',4,'1/4/2020','female'))
+   array.push(student('ana',5,'1/5/2020','female'))
+
+   ////////////Create a function that takes a student as parameter and print out the details of the student
+   
